9b05f60c615599b3ba8cff982de08ef999f84ff4
agcarte4 <42975089+agcarte4@users.noreply.github.com>
2018-10-17 12:19:37 -0400

    Update hw6a.cpp
    removed main

--- lib/hw6a.cpp
+++ lib/hw6a.cpp
@@ -57,13 +57,3 @@ bool IntegerSetHT::insert(int data)
    failures ++;
    return false;
 }
-int main()
-{
-    IntegerSetHT a(1000);
-    srand(time(NULL));
-
-    for(int i = 0; i<500; i++)
-        a.insert(rand());
-    printf("\nCollisons = %d, Failures = %d",a.collisions, a.failures);
-
-}
