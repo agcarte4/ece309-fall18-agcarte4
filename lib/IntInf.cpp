0e28dafc16ff978e05dee2af3f3d825229a844ab
agcarte4 <42975089+agcarte4@users.noreply.github.com>
2018-12-03 20:19:45 -0500

    Update IntInf.cpp

--- lib/IntInf.cpp
+++ lib/IntInf.cpp
@@ -1,13 +1,9 @@
 #include <stdio.h>
+#include "IntInf.h"
 
 namespace ece309 {
 
 class IntInf {
-public:                 //public for convenience
-    bool pos_inf;       //set to true to represent positive infinity
-    bool neg_inf;       //set to true to represent negative infinity
-    int num;            //set to a integer value to represent an int
-    bool empty;         //set true if this object is created with no parameter
 public:
     IntInf() {          //default constructor creates an empty object
         pos_inf = false; 
